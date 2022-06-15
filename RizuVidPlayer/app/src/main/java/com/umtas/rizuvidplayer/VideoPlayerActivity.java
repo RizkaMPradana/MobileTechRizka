f1475ca252ff86483bd8a70a446d9be19fa4bc2c
RizkaMPradana <93360411+RizkaMPradana@users.noreply.github.com>
2022-06-15 16:45:55 +0700

    Menambahkan UI reverse, play, forward, bar durasi dan lainnya
    Melanjutkan pekan lalu

--- RizuVidPlayer/app/src/main/java/com/umtas/rizuvidplayer/VideoPlayerActivity.java
+++ RizuVidPlayer/app/src/main/java/com/umtas/rizuvidplayer/VideoPlayerActivity.java
@@ -2,13 +2,136 @@ package com.umtas.rizuvidplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
+    private TextView videoNameTV, videoTimeTV;
+    private ImageButton backIB, forwardIB, playPauseIB;
+    private SeekBar videoSeekBar;
+    private VideoView videoView;
+    private RelativeLayout controlsRL, videoRL;
+    boolean isOpen = true;
+    private String videoName, videoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        videoName = getIntent().getStringExtra("videoName");
+        videoPath = getIntent().getStringExtra("videoPath");
+        videoNameTV = findViewById(R.id.idTVVideoTitle);
+        videoTimeTV = findViewById(R.id.idTVTime);
+        backIB = findViewById(R.id.idIBBack);
+        playPauseIB = findViewById(R.id.idIBPlay);
+        forwardIB = findViewById(R.id.idIBForward);
+        videoSeekBar= findViewById(R.id.idSeekBarProgress);
+        videoView = findViewById(R.id.idVideoView);
+        controlsRL = findViewById(R.id.idRLControls);
+        videoRL = findViewById(R.id.idRLVideo);
+
+        videoView.setVideoURI(Uri.parse(videoPath));
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoSeekBar.setMax(videoView.getDuration());
+                videoView.start();
+            }
+        });
+
+        videoNameTV.setText(videoName);
+        backIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoView.seekTo(VideoView.getDuration()-10000);
+            }
+        });
+        forwardIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoView.seekTo(VideoView.getDuration()+10000);
+            }
+        });
+        playPauseIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(videoView.isPlaying()){
+                    videoView.pause();
+                    playPauseIB.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
+                }else{
+                    videoView.start();
+                    playPauseIB.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause));
+                }
+            }
+        });
+        videoRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                    hideControls();
+                    isOpen = false;}else{
+                    showControls();
+                    isOpen = true;
+                }
+            }
+        });
+    }
+
+    private void showControls() {
+        controlsRL.setVisibility(View.VISIBLE);
+        final Window window = this.getWindow();
+        if(window==null){
+            return;
+        }
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        View decorView = window.getDecorView();
+        if(decorView!=null){
+            int uiOption = decorView.getSystemUiVisibility();
+            if(Build.VERSION.SDK_INT>=14){
+                uiOption&= ~View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+            if(Build.VERSION.SDK_INT>=16){
+                uiOption&= ~View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+            if(Build.VERSION.SDK_INT>=19){
+                uiOption&= ~View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+            decorView.setSystemUiVisibility(uiOption);
+        }
+    }
+
+    private void hideControls() {
+        controlsRL.setVisibility(View.GONE);
+        final Window window = this.getWindow();
+        if(window==null){
+            return;
+        }
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        View decorView = window.getDecorView();
+        if(decorView!=null){
+            int uiOption = decorView.getSystemUiVisibility();
+            if(Build.VERSION.SDK_INT>=14){
+                uiOption |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+            if(Build.VERSION.SDK_INT>=16){
+                uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+            if(Build.VERSION.SDK_INT>=19){
+                uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+            decorView.setSystemUiVisibility(uiOption);
+        }
     }
 }
